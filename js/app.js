b0d73c0d0b1f66ed986892dd3fe2479844dc804d
jacobrigal <webdevjacobrigal@gmail.com>
2020-06-17 01:30:22 -0500

    Added five questions, logic, name prompt.

---
+++ js/app.js
@@ -0,0 +1,80 @@
+'use strict';
+
+//Check file is connected
+
+//console.log('this file is connected');
+
+// //convert the input to lowerCase
+
+var upperCaseName = userName.toUpperCase();
+
+// console.log(lowerCaseName);
+
+
+//Prompt user for name
+
+var userName = prompt('What\'s your name?','');
+
+//Greet user
+
+alert('hi ' + upperCaseName + '!');
+
+//first question
+
+var quest1 = prompt('Question 1: Was I born in Iowa?', '');
+var lowerCaseQuest1 = quest1.toLowerCase();
+
+if (lowerCaseQuest1 === ('yes')) {
+  alert('Nope. I was born in Palo Alto, CA.');
+}
+else if (lowerCaseQuest1 === ('no')) {
+  alert('Correct! I was born in Stanford General hospital in Palo Alto, CA. The same doctor that delivered Dan Marino\'s son delivered me...so I\'m basically famous!');
+}
+else alert('Please enter yes or no');
+
+var lowerCaseQuest2 = quest2.toLowerCase();
+var quest2 = prompt('Question 2: Have I been to China?', 'yes or no');
+
+if (lowerCaseQuest2 === ('yes')) {
+  alert('Nice try, but no. I do teach Chinese kids English online, though.');
+}
+if (lowerCaseQuest2 === ('no')) {
+  alert('Right on. I do telecommute there as superhero  \'Teacher Jacob.\' He\'s just your friendly neighborhood online ESL teacher.');
+}
+
+var lowerCaseQuest3 = quest3.toLowerCase();
+var quest3 = prompt('Question 3: Do I like whole milk?', 'yes or no');
+
+if (lowerCaseQuest3 === ('yes')) {
+  alert('Blah, no! I\'m lactose intolerant!');
+}
+
+if (lowerCaseQuest3 === ('no')) {
+  alert('Subtle skills, my hu-man! Yeah, I\'m more of a coconut milk kind of guy.');
+}
+
+var lowerCaseQuest4 = quest4.toLowerCase();
+var quest4 = prompt('Question 4: Is my favorite book the Bible?', 'yes or no');
+
+if (lowerCaseQuest4 === ('yes')) {
+  alert('That\'s correct! I prefer to read it in the original language: Hebrew');
+}
+if (lowerCaseQuest4 === ('no')) {
+  alert('Easy mistake. Still, it\'s the #1 bestseller of all time for a reason. Don\'t believe the anti-hype! At the very, very least it\'s highly relevant to our world!');
+}
+
+var lowerCaseQuest5 = quest5.toLowerCase();
+var quest5 = prompt('Question 5: Do I speak French?', 'oui or non');
+
+if (lowerCaseQuest5 === ('oui')) {
+  alert('Oui, oui!');
+}
+else if (lowerCaseQuest5 === ('non')) {
+  alert('Mais, oui! But, yes!');
+}
+else {
+  prompt('please enter yes or no in French!', 'oui or non');
+}
+
+alert('Thanks,' + userName); 
+}
